7ed387c6b71ba1902b249a8da34d081eab05d2d0
inverseAgentSmith <theonewhocodes10@gmail.com>
2022-07-09 02:15:50 +0200

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by neo on 2022/07/09.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
