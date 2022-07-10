8d47e9c3f8e56f447769eaf58f71b8dae229f13f
inverseAgentSmith <theonewhocodes10@gmail.com>
2022-07-11 01:54:49 +0200

    added colour assets for app themes

---
+++ Scrumdinger/Models/Themes.swift
@@ -0,0 +1,29 @@
+//
+//  Themes.swift
+//  Scrumdinger
+//
+//  Created by neo on 2022/07/11.
+//
+
+import SwiftUI
+
+enum Theme: String {
+    
+    case bubblegum
+    case buttercup
+    case indigo
+    case lavender
+    case magenta
+    case navy
+    case orange
+    case oxblood
+    case periwinkle
+    case poppy
+    case purple
+    case seafoam
+    case sky
+    case tan
+    case teal
+    case yellow
+    
+}
