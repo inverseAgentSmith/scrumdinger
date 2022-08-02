7680b699748716322d01e62096d2eba29782935c
inverseAgentSmith <theonewhocodes10@gmail.com>
2022-08-03 01:15:18 +0200

    added attendees section

--- Scrumdinger/DetailEditView.swift
+++ Scrumdinger/DetailEditView.swift
@@ -15,18 +15,23 @@ struct DetailEditView: View {
         Form {
             Section(header: Text("Meeting Info")) {
                 TextField("Title", text: $data.title)
-            }
-            HStack {
-                Slider(value: $data.meetingLengthInMinutes, in: 5...30, step: 1) {
-                    Text("Length")
+                
+                HStack {
+                    Slider(value: $data.meetingLengthInMinutes, in: 5...30, step: 1) {
+                        Text("Length")
+                    }
+                    Spacer()
+                    Text("\(Int(data.meetingLengthInMinutes)) minutes")
                 }
-                Spacer()
-                Text("\(Int(data.meetingLengthInMinutes)) minutes")
+            }
+            Section(header: Text("Attendees")) {
+                ForEach(data.attendees) { attendee in
+                    Text(attendee.name)
+                }
             }
         }
     }
 }
-
 struct DetailEditView_Previews: PreviewProvider {
     static var previews: some View {
         DetailEditView()
